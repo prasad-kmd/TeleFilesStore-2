237e72f3757603eb8de0886ca56ddeae6a98948c
PRASAD MADHURANGA <102537177+prasad-kmd@users.noreply.github.com>
2022-08-19 18:13:21 +0530

    Links Added !

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ Creator : <a href='https://t.me/+nuPiMH77sNE2ZWVl'>SINHAGIRI VISUAL STUDIO™</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Cloned Source Code : <a href='https://github.com/prasad-kmd/TeleFilesStore-2'>TeleFilesStore-2</a>\n○ Channel : @sinhagiri_visual_studio\n○ Support Group : <a href='https://t.me/+JfwbBo_nlnNkZmU9'>@games_softwares_community</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
